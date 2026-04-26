c0e894a8e6b4782d9c87236378998dd2aca5ca8a
gabriel-cvt <gabriel.cavalcanti.araujo@ccc.ufcg.edu.br>
2026-04-26 10:44:24 -0300

    feat(backend): add produto model and repository

---
+++ backend/src/main/java/com/minimercado/backend/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.minimercado.backend.repository;
+
+
+import com.minimercado.backend.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+}
